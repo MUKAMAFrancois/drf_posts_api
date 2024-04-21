e35c1b2170c5a6d752608a2754ba4e164463f9a6
Mukama <mk.frmh3@gmail.com>
2024-04-21 19:04:12 +0200

    whitenoise

--- drfCourse1/settings.py
+++ drfCourse1/settings.py
@@ -58,6 +58,7 @@ AUTH_USER_MODEL = 'accounts.User'#
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,6 +142,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT= os.path.join(BASE_DIR,'assets')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
